ea572db73fa3e4f51ba92f5eebb8786ae8298183
hbrooks3 <hunterbrooks@outlook.com>
2020-06-01 14:07:25 -0500

    Cleaned up UI

--- src/components/FormsList.js
+++ src/components/FormsList.js
@@ -5,13 +5,23 @@ import { Link } from "@reach/router";
 import { useSelector, useDispatch } from "react-redux";
 import { createForm } from "actions";
 
+// bootstrap
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
 // Remove hardcode later
 const userId = "1234";
 
 const FormCard = ({ title, formId }) => (
-  <li key={formId}>
-    <Link to={`/${formId}`}>{title}</Link>
-  </li>
+  <Card key={formId} style={{ width: "100%" }} className="mb-3">
+    <Card.Body>
+      <Card.Title>
+        <Link to={`/${formId}`}>{title}</Link>
+      </Card.Title>
+    </Card.Body>
+  </Card>
 );
 
 const FormsList = () => {
@@ -22,11 +32,20 @@ const FormsList = () => {
   };
 
   return (
-    <div>
-      <h1>FormsList</h1>
-      <ul>{forms.map(FormCard)}</ul>
-      <button onClick={handleClick(userId)}>Add form</button>
-    </div>
+    <Container>
+      <Row className="mt-2 mb-4">
+        <h1>Form Manager</h1>
+        <Button
+          variant="outline-primary"
+          onClick={handleClick(userId)}
+          className="ml-auto"
+          size="sm"
+        >
+          CREATE A NEW FORM
+        </Button>
+      </Row>
+      <Row>{forms.map(FormCard)}</Row>
+    </Container>
   );
 };
 
